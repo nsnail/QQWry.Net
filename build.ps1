1a82dd14d70f4723a64e840fc54d41778e573d65
tk <taokeu@gmail.com>
2023-09-27 13:13:46 +0800

    [BLD] [SKIP CI] 构建脚本

--- build.ps1
+++ build.ps1
@@ -1,3 +1,5 @@
 dotnet publish -c Release -r win-x64 --sc /p:PublishSingleFile=true /p:EnableCompressionInSingleFile=true -o ./dist/QQWry.Net/win-x64
 dotnet publish -c Release -r linux-x64 --sc /p:PublishSingleFile=true /p:EnableCompressionInSingleFile=true -o ./dist/QQWry.Net/linux-x64-singlefile
 dotnet publish -c Release -r linux-x64 -o ./dist/QQWry.Net/linux-x64
+docker build . -t nsnail2/qqwry.net
+docker push nsnail2/qqwry.net
