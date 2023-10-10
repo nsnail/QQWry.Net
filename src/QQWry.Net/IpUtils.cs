842fcee6262dd4746ffd62311ce98c2e7eb77eda
tk <taokeu@gmail.com>
2023-10-10 15:56:01 +0800

    [FEA] [SKIP CI] IP数据库更新至20231004

--- src/QQWry.Net/IpUtils.cs
+++ src/QQWry.Net/IpUtils.cs
@@ -19,7 +19,7 @@ internal sealed class IpUtils
     {
         Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
         using var sr = new StreamReader( //
-            Assembly.GetExecutingAssembly().GetManifestResourceStream("QQWry.Net.qqwrt-20230920.txt")!
+            Assembly.GetExecutingAssembly().GetManifestResourceStream("QQWry.Net.qqwrt-20231004.txt")!
           , Encoding.GetEncoding("gbk"));
 
         while (!sr.EndOfStream) {
